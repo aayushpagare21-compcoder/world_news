745d20443eacecbf37ab859220976e87691309e1
Aayush <aayushpagare21@gmail.com>
2021-09-06 13:18:03 +0530

    THIRD COMMIT

--- app.js
+++ app.js
@@ -12,18 +12,22 @@ xhr.open('GET', `https://gnews.io/api/v4/top-headlines?token=c448bbd4773130406e1
 `, true);
 
 xhr.onload = function () {
-    let json = JSON.parse(this.responseText);
-    let articles = json.articles;
-    
-    let newsHtml = ``; 
 
-    articles.forEach(function(element, index) {
-        console.log(element); 
-        let news =  
-        `<div class="card">
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+
+        console.log(this.responseText);
+
+        let newsHtml = ``;
+
+        articles.forEach(function (element, index) {
+            console.log(element);
+            let news =
+                `<div class="card">
             <div class="card-header" id="heading${index}">
                 <h2 class="mb-2 heading">
-                    <button class="btn collapsed" type="button" data-toggle="collapse"
+                    <button class="btn collapsed head" type="button" data-toggle="collapse"
                         data-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
                         ${element['title']}
                     </button>
@@ -36,13 +40,16 @@ xhr.onload = function () {
                     ${element['content']} <a href = ${element['url']}> Read More </a>
                 </div>
             </div>
-        </div>`; 
+        </div>`;
 
-        newsHtml += news;
-    }); 
+            newsHtml += news;
+        });
 
-    newsAccordion.innerHTML = newsHtml;
+        newsAccordion.innerHTML = newsHtml;
+    } else { 
+        console.log('Some error occured'); 
+    }
 }
-
+ //pub_1121b3820c1a36f58494202f9b9cdcf8d001
 xhr.send();
 
